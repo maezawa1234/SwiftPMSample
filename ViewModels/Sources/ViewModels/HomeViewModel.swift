3d56ab4c7ad5e520ed4f0bbd5ccf0d59cd420592
KenichiMaezawa <maezawa0987@gmail.com>
2022-09-05 05:06:18 +0900

    APIKit

--- ViewModels/Sources/ViewModels/HomeViewModel.swift
+++ ViewModels/Sources/ViewModels/HomeViewModel.swift
@@ -1,9 +1,24 @@
 import Foundation
 import RxSwift
+import APIKit
+
+struct TestRequest: APIKit.Request {
+    var baseURL: URL = URL(string: "")!
+    var method: HTTPMethod = .get
+    var path: String = ""
+
+    func response(from object: Any, urlResponse: HTTPURLResponse) throws -> String {
+        "test"
+    }
+
+    typealias Response = String
+}
 
 public class HomeViewModel {
     private let disposeBag = DisposeBag()
 
-    public init() {}
+    public init() {
+    }
     public let text: String = "Hello world!"
+
 }
