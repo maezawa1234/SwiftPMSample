b106980a3dffcbb520ca83edfd933fbd130d7b4e
KenichiMaezawa <maezawa0987@gmail.com>
2022-09-05 14:40:01 +0900

    fix

--- View/Package.swift
+++ View/Package.swift
@@ -8,23 +8,26 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "View",
+            //name: "View",
+            name: "AppFeature",
             targets: ["View", "ViewModel", "Repositories"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/ReactiveX/RxSwift", from: "6.5.0"),
+        .package(url: "https://github.com/ishkawa/APIKit", from: "5.3.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "View",
-            dependencies: ["ViewModel", "Repositories"]),
+            dependencies: ["ViewModel", "Repositories", "RxSwift", "APIKit"]),
         .target(name: "ViewModel",
-               dependencies: []),
+               dependencies: ["RxSwift", "APIKit"]),
         .target(name: "Repositories",
-               dependencies: []),
+               dependencies: ["RxSwift", "APIKit"]),
         .testTarget(
             name: "ViewTests",
             dependencies: ["View"]),
